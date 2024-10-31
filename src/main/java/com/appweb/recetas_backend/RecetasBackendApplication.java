25e7fd79447489a65b2cb67fae99d2b5ff217b8c
Manuel Valdés <manuelvaldesg97@gmail.com>
2024-10-31 13:10:14 -0300

    Creación proyecto Java Spring Boot con Oracle Cloud

---
+++ src/main/java/com/appweb/recetas_backend/RecetasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.appweb.recetas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appweb/recetas_backend/RecetasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appweb.recetas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
